decc4056ca8c9dcff3711890e8d36999fc1001e1
Harshala <mharshala334@gmail.com>
2025-11-08 19:25:12 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,26 @@
 //import React from 'react'
-import { BrowserRouter,Route,Routes} from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Signin from "./pages/Signin"
-import Signup from "./pages/Signup"
-import Dashboard from "./pages/Dashboard"
-import Projects from "./pages/Projects"
-import Header from "./components/Header"
-import FooterComp from "./components/Footer"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterComp from "./components/Footer";
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/signin" element={<Signin/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/projects" element={<Projects/>}/>
-      
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/projects" element={<Projects/>} />
       </Routes>
       <FooterComp/>
-      </BrowserRouter>
-  )
+    </BrowserRouter>
+  );
 }
